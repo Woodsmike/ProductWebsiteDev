ecc2ddd4d00b1b0998946ac278b733792850de13
Woodsmike <Woodsmike@users.noreply.github.com>
2017-07-19 23:23:50 -0400

    final with some javascript

---
+++ Jscript.js
@@ -0,0 +1,8 @@
+﻿function myFunction() {
+    var x = document.getElementById("navId");
+    if (x.className === "navbar navbar-static-top") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar navbar-static-top";
+    }
+}
